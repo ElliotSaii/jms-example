46db963abd98e71269fe5a9deb419a8e357db832
afu <elliotsaii1@gmail.com>
2022-12-20 21:05:41 +0800

    initial project

---
+++ src/main/java/com/techguy/JmsMsgApplication.java
@@ -0,0 +1,13 @@
+package com.techguy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsMsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsMsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techguy/JmsMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techguy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsMsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
